4ef5fdf19c4276f1a90d3fc1482d098bc5b8bc92
datnt@gm-group.vn <datnt@gm-group.vn>
2022-11-19 23:22:02 +0700

    create project

---
+++ src/test/java/com/crawl/api/InfomationCrawlSystermApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawl.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfomationCrawlSystermApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
