fee2f6637d3e99ec515e33de8a07647cdf68a39c
datnt@gm-group.vn <datnt@gm-group.vn>
2022-12-04 11:51:24 +0700

    add file config thymeleaf

--- src/main/java/com/crawl/api/InfomationCrawlSystermApiApplication.java
+++ src/main/java/com/crawl/api/InfomationCrawlSystermApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InfomationCrawlSystermApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InfomationCrawlSystermApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(InfomationCrawlSystermApiApplication.class, args);
+  }
 
 }
--- src/main/java/com/crawl/api/controller/Hacom.java
+++ src/main/java/com/crawl/api/controller/Hacom.java
@@ -7,6 +7,7 @@ import com.crawl.api.services.Batch1Service;
 import com.crawl.api.services.Batch2Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("hacom")
@@ -26,5 +27,10 @@ public class Hacom {
     public final String filterUrlForBatch3(@RequestBody RequestFilterUrlBatch2Dto dto){
         batch2Service.Batch3UrlFilter(dto);
         return "complete filter url batch 3";
+    }
+
+    @GetMapping("filter/hello")
+    public final ModelAndView hello(){
+        return new ModelAndView("filter/Batch2Filter");
     }
 }
