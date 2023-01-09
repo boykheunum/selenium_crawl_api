eab5e0b82cde84b706154d21f119dc41a885fba4
boykheunum <tiendat28121999@gmail.com>
2023-01-09 22:39:05 +0700

    update comment for chart

--- src/main/resources/static/assets/js/CustomChart.js
+++ src/main/resources/static/assets/js/CustomChart.js
@@ -18,6 +18,7 @@ function dataForChart(data) {
     let ncpc = [];
     const priceLabels = [5000000, 10000000, 15000000, 20000000, 25000000, 30000000, 40000000, 10000000000];
     let str = "";
+    //chia data dau vao thanh cua hacom va ncpc
     for (let i = 0; i < data.length; i++) {
         data[i].executionId = parseInt(data[i].executionId);
         data[i].price = numberFormatPrice(data[i].price);
@@ -34,10 +35,13 @@ function dataForChart(data) {
     console.log(hacom);
     console.log("ncpc  ");
     console.log(ncpc);
+    //set datasets cua bieu do thanh rong
     myLineChart.data.datasets[0].data = [];
     myLineChart.data.datasets[1].data = [];
+    //chia thanh 8 khoang gia
     let hacomViews = [0, 0, 0, 0, 0, 0, 0, 0];
     let ncpcViews = [0, 0, 0, 0, 0, 0, 0, 0];
+    // tinh tong view theo moi khoang gia hacom
     for (let i = 0; i < hacom.length; i++) {
         for(let j = 0; j < priceLabels.length; j++) {
             if(hacom[i].price < priceLabels[j]) {
@@ -46,6 +50,7 @@ function dataForChart(data) {
             }
         }
     }
+    // tinh tong view theo moi khoang gia ncpc
     for (let i = 0; i < ncpc.length; i++) {
         for(let j = 0; j < priceLabels.length; j++) {
             if(ncpc[i].price < priceLabels[j]) {
@@ -54,12 +59,13 @@ function dataForChart(data) {
             }
         }
     }
+    //update data moi de ve bieu do
     for(let i = 0; i < priceLabels.length; i++) {
         myLineChart.data.datasets[0].data.push(hacomViews[i]);
         myLineChart.data.datasets[1].data.push(ncpcViews[i]);
     }
     myLineChart.update();
-
+    //set data null cho bieu do bar cua hacom
     hacomBar.data.labels = [];
     hacomBar.data.datasets[0].data = [];
     hacom.sort(function (a, b) {return b.price - a.price});
@@ -74,6 +80,7 @@ function dataForChart(data) {
     }
     hacomBar.update();
 
+    //set data null cho bieu do bar cua nguyen cong
     ncpcBar.data.labels = [];
     ncpcBar.data.datasets[0].data = [];
     ncpc.sort(function (a, b) {return b.price - a.price});
@@ -92,9 +99,7 @@ function dataForChart(data) {
 var hacomBar = new Chart(bar1, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            data: [12, 19, 3, 5, 2, 3],
             borderWidth: 1
         }]
     },
@@ -122,9 +127,7 @@ var hacomBar = new Chart(bar1, {
 var ncpcBar = new Chart(bar2, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            data: [12, 19, 3, 5, 2, 3],
             borderWidth: 1
         }]
     },
@@ -150,22 +153,6 @@ var ncpcBar = new Chart(bar2, {
     }
 });
 
-// let lineChartDemo = new Chart(line, {
-//     type: "line",
-//     data: {
-//         datasets:[{
-//             backgroundColor:'rgb(200,200,198)',
-//             backgroundColor:'rgb(200,200,198)',
-//             data:[1,2,3,4,5]
-//         },
-//         {
-//             backgroundColor:'rgb(20,78,198)',
-//             backgroundColor:'rgb(20,78,198)',
-//             data:[1,4,3,8,10]
-//         }
-//     ]
-//     },
-// })
 
 var myLineChart = new Chart(line, {
     type: 'line',
@@ -176,26 +163,12 @@ var myLineChart = new Chart(line, {
             label: "HACOM",
             backgroundColor: "rgba(78, 115, 223, 0.05)",
             borderColor: "rgba(78, 115, 223, 1)",
-            pointRadius: 3,
-            pointBackgroundColor: "rgba(78, 115, 223, 1)",
-            pointBorderColor: "rgba(78, 115, 223, 1)",
-            pointHoverRadius: 3,
-            pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-            pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-            pointHitRadius: 10,
-            pointBorderWidth: 2,
+
         }, {
             label: "NCPC",
             backgroundColor: "rgba(208, 15, 23, 0.05)",
             borderColor: "rgba(208, 15, 23, 1)",
-            pointRadius: 3,
-            pointBackgroundColor: "rgba(208, 15, 23, 1)",
-            pointBorderColor: "rgba(208, 15, 23, 1)",
-            pointHoverRadius: 3,
-            pointHoverBackgroundColor: "rgba(208, 15, 23, 1)",
-            pointHoverBorderColor: "rgba(208, 15, 23, 1)",
-            pointHitRadius: 10,
-            pointBorderWidth: 2,
+
         }],
     },
     options: {
@@ -230,21 +203,6 @@ var myLineChart = new Chart(line, {
         },
         legend: {
             display: false
-        },
-        tooltips: {
-            backgroundColor: "rgb(255,255,255)",
-            bodyFontColor: "#858796",
-            titleMarginBottom: 10,
-            titleFontColor: '#6e707e',
-            titleFontSize: 14,
-            borderColor: '#dddfeb',
-            borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
-            displayColors: false,
-            intersect: false,
-            mode: 'index',
-            caretPadding: 10
         }
     }
 });
