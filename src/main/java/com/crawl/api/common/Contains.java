e2d4375d0a6fd1a0ca93ad305744581be4f43af6
datnt@gm-group.vn <datnt@gm-group.vn>
2023-02-01 22:42:00 +0700

    update template chart

--- src/main/java/com/crawl/api/common/Contains.java
+++ src/main/java/com/crawl/api/common/Contains.java
@@ -2,7 +2,7 @@ package com.crawl.api.common;
 
 public class Contains {
     public static final class Batch1{
-        public static final int LIMIT_URL_FOR_BATCH_2 = 1000;
+        public static final int LIMIT_URL_FOR_BATCH_2 = 10000;
     }
     public static final class TypeRobot{
         public static final int TYPE_HACOM_BATCH1 = 1;
